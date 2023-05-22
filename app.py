b8868747ceee1209f1154fe7c8c88bffb0a134cb
JacopoBiagioni <90761226+JacopoBiagioni@users.noreply.github.com>
2023-05-22 09:45:48 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,23 +6,41 @@
 import tkinter as tk 
 import pymssql
 
-window = tk.Tk()
+conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='biagioni.jacopo', password='xxx123##', database='biagioni.jacopo') 
 
+window = tk.Tk()
 window.geometry("600x600")
 window.title("Hello TkInter!")
-
 window.configure(background="white")
+
+username_sql = tk.Entry(window)
+password_sql = tk.Entry(window)
 
 def second_function():
     text = "Nuovo Messaggio! Nuova Funzione!"
     text_output = tk.Label(window, text=text, fg="green", font=("Helvetica", 16))
     text_output.grid(row=1, column=1, padx=50, sticky="W")
 
-
 def ciao_utente():
     text = box.get()
     text_output = tk.Label(window, text = text, fg="red", font=("Helvetica", 16))
     text_output.grid(row=0, column=3, sticky="W")
+
+def InsertUsers():
+    conn.cursor()
+    cursor.execute = 'INSERT INTO Utenti VALUES (%s, %s)', (username_sql, password_sql)
+
+    conn.commit()
+    conn.close()
+
+    button = tk.Button(window, text='Inserisci', command=InsertUsers)
+    button.pack()
+
+def DeleteUsers():
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM Utenti WHERE Username=%(username_sql)s and Password=%(password)s')
+    conn.commit()
+    conn.close()
 
 userInput = tk.Label(window, text='Username').grid(row=0)   
 box = tk.Entry(window)
