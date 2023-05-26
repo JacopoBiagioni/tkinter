4c13ff75507aad14b25939467919ba38910f1eea
JacopoBiagioni <biagioni.jacopo@iisgalvanimi.edu.it>
2023-05-26 08:55:23 +0000

    ok

--- app.py
+++ app.py
@@ -13,33 +13,29 @@ window.geometry("600x600")
 window.title("Hello TkInter!")
 window.configure(background="white")
 
-username_sql = tk.Entry(window)
-password_sql = tk.Entry(window)
-username2_sql = tk.Entry(window)
-password2_sql = tk.Entry(window)
-ricerca_sql = tk.Entry(window)
+#username_sql = tk.Entry(window)
+#password_sql = tk.Entry(window)
+#username2_sql = tk.Entry(window)
+#password2_sql = tk.Entry(window)
+#ricerca_sql = tk.Entry(window)
 
-def second_function():
-    text = "Nuovo Messaggio! Nuova Funzione!"
-    text_output = tk.Label(window, text=text, fg="green", font=("Helvetica", 16))
-    text_output.grid(row=1, column=1, padx=50, sticky="W")
-
-def ciao_utente():
-    text = box.get()
-    text_output = tk.Label(window, text = text, fg="red", font=("Helvetica", 16))
-    text_output.grid(row=0, column=3, sticky="W")
 
 def InsertUsers():
+    username_sql = tk.Entry(window)
+    password_sql = tk.Entry(window)
     conn.cursor()
-    cursor.execute = 'INSERT INTO Utenti VALUES %(username_sql)s, %(password_sql)s'
+    cursor.execute = ("INSERT INTO Utenti VALUES (%(username_sql)s, %(password_sql)s)")
 
     conn.commit()
     conn.close()
 
-    button = tk.Button(window, text='Inserisci', command=InsertUsers)
-    button.pack()
+
+button = tk.Button(window, text='Inserisci', command=InsertUsers)
+button.pack()
 
 def DeleteUsers():
+    username_sql = tk.Entry(window)
+    ùpassword_sql = tk.Entry(window)
     cursor = conn.cursor()
     cursor.execute('DELETE FROM Utenti WHERE Username=%(username_sql)s and Password=%(password)s')
     conn.commit()
@@ -49,6 +45,8 @@ def DeleteUsers():
     button2.pack()
 
 def ModificaUsers():
+    username_sql = tk.Entry(window)
+    username2_sql = tk.Entry(window)
     cursor = conn.cursor()
     cursor.execute('UPDATE Utenti SET Username=%(username2_sql)s WHERE Username=%(username_sql)s')
     conn.commit()
@@ -58,6 +56,8 @@ def ModificaUsers():
     button3.pack()
 
 def ModificaPassword():
+    password_sql = tk.Entry(window)
+    password2_sql = tk.Entry(window)
     cursor = conn.cursor()
     cursor.execute('UPDATE Utenti SET Password=%(password2_sql)s WHERE Password=%(password_sql)s')
     conn.commit()
@@ -67,6 +67,7 @@ def ModificaPassword():
     button4.pack()
 
 def Ricerca():
+
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Utenti WHERE Username or Password LIKE %'%(ricerca_sql)s'%")
     conn.commit()
@@ -75,15 +76,12 @@ def Ricerca():
     button5 = tk.Button(window, text='Ricerca', command=Ricerca)
     button5.pack()
 
-userInput = tk.Label(window, text='Username').grid(row=0)   
-box = tk.Entry(window)
-box.grid(row=0, column=1, sticky='W')
+ButtonRicerca = tk.Button(window, text='Ricerca', command=Ricerca)
+ButtonInserisci = tk.Button(window, text='Inserisci', command=InsertUsers)
+ButtonElimina = tk.Button(window, text='Elimina', command=DeleteUsers)
+ButtonModUser = tk.Button(window, text='Modifica Utente', command=ModificaUsers)
+ButtonModPass = tk.Button(window, text='Modifica Password', command=ModificaPassword)
 
-userButton = tk.Button(text='Username', command=ciao_utente)
-userButton.grid(row=0, column=2, )
-
-second_button = tk.Button(text="Seconda Funzione", command=second_function)
-second_button.grid(row=1, column=0, pady=20, sticky="W")
 
 
 if __name__ == "__main__":
